f9a1b8f2cedf5dba0f5a0cdd927ae10e7fbba58c
childsafetyawarenesswa-lgtm <childsafetyawarenesswa@gmail.com>
2026-02-18 13:39:10 +0800

    Update childsafety-news.ts

--- routes/wa-child-safety/childsafety-news.ts
+++ routes/wa-child-safety/childsafety-news.ts
@@ -1,1 +1,22 @@
+import { Route } from '@/types';
+
+export const route: Route = {
+  path: '/wa-child-safety/childsafety-news',
+  name: 'ChildSafety.gov.au - News (POC)',
+  example: '/wa-child-safety/childsafety-news',
+  maintainers: ['scott'],
+  handler: async (ctx) => {
+    ctx.state.data = {
+      title: 'POC feed - ChildSafety news',
+      link: 'https://www.childsafety.gov.au/news',
+      item: [
+        {
+          title: 'Test item',
+          link: 'https://www.childsafety.gov.au/news',
+          description: 'If you can see this, your custom route is wired correctly.',
+        },
+      ],
+    };
+  },
+};
 
